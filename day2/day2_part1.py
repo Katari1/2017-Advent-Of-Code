553d28fd59e60c595fdb6dab25869b7b121707cb
Vikram <vnagrani@pindropsecurity.com>
2017-12-03 22:31:00 -0500

    Added Day2

---
+++ day2/day2_part1.py
@@ -0,0 +1,26 @@
+def parse_input(puzzle_input):
+    data=[]
+    content = open(puzzle_input,'r')
+    lines = content.readlines()
+    for l in lines:
+        if l.strip():
+            l = (l)
+            data.append(l.strip().split('\t'))
+    return data
+
+a = parse_input('day2.txt')
+sum = 0
+for i in a:
+    diff = 0
+    ints=[]
+    for z in i:
+        ints.append(int(z))
+        ints = sorted(set(ints))
+    lowest = ints[0]
+    highest = ints[-1]
+    print "The highest number is: %s" %(highest),
+    print "The lowest number is %s" %(lowest),
+    diff = (highest - lowest)
+    sum += diff
+    print "The difference is %s" %(diff)
+print sum
---
+++ day2/day2_part2.py
@@ -0,0 +1,28 @@
+def parse_input(puzzle_input):
+    data=[]
+    content = open(puzzle_input,'r')
+    lines = content.readlines()
+    for l in lines:
+        if l.strip():
+            l = (l)
+            data.append(l.strip().split('\t'))
+    return data
+
+a = parse_input('day2.txt')
+sum = 0
+for i in a:
+    diff = 0
+    ints=[]
+    for z in i:
+        ints.append(int(z))
+        ints = sorted(set(ints))
+    for z in range(len(ints)):
+        #print ints[z],
+        for y  in ints:
+            if ints[z] == y:
+                pass
+            elif ints[z] % y == 0:
+                print ints[z],
+                print y
+                sum += ints[z] / y
+print sum
